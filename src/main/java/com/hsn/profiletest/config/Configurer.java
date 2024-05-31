1f7acd7ddd3c54ecbf255cbace1b1d993d7d3a01
hasan-alam <hasan.bin.alam.tanim@gmail.com>
2024-06-01 00:11:04 +0600

    updated

--- src/main/java/com/hsn/profiletest/config/Configurer.java
+++ src/main/java/com/hsn/profiletest/config/Configurer.java
@@ -1,12 +1,6 @@
 package com.hsn.profiletest.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import com.hsn.profiletest.component.DevPocessor;
-import com.hsn.profiletest.component.Processor;
-import com.hsn.profiletest.component.UatProcessor;
 
 @Configuration
 public class Configurer {
